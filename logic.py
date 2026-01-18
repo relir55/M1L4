81894cb3ac5281d2d06b20ea939841bd5fb3bc00
relir55 <pashaananin70@gmail.com>
2026-01-18 19:29:50 +0500

    Комментарий к коммиту

--- logic.py
+++ logic.py
@@ -11,12 +11,22 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.pokemon_height = self.get_height()
+        self.hp = randint(70, 200)
+        self.power = randint(5, 20)
 
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
     def get_img(self):
-        pass
+            url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                return (data['sprites']['other']['official-artwork']["front_shiny"])
+            else:
+                return 'https://avatars.mds.yandex.net/get-entity_search/10783309/1219900500/S600xU_2x'
+
     
     # Метод для получения имени покемона через API
     def get_name(self):
@@ -27,15 +37,51 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
+        
+    def get_height(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['height'])
+        else:
+            return "Pikachu"
+        
+    
+
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
 
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покеомона: {self.name}, рост: {self.pokemon_height} м, сила: {self.power}, здоровье: {self.hp}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
 
+class Wizard (Pokemon):
+    pass
+
+
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5, 15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
 
 
